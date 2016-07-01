47b51a40d50230562487d68662a07584283d0729
Emil Persson <emil.persson@creuna.se>
2016-07-01 18:44:07 +0200

    Set up build

---
+++ eliot.config.js
@@ -0,0 +1,16 @@
+import { Target } from 'eliot/config'
+
+export default {
+  targets: [
+    {
+      target: Target.NODE6,
+      entry: 'src/server/index.js',
+      output: 'build/server.js'
+    },
+    {
+      target: Target.ES5,
+      entry: 'src/client/index.js',
+      output: 'build/client.js'
+    }
+  ]
+}
